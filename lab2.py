8bcd7f02acbaa312fe92868babcbc0f733d7f69f
Devin Haynes <S_devin.haynes@ousd.org>
2021-06-28 11:33:30 -0700

    done

--- lab2.py
+++ lab2.py
@@ -1,1 +1,10 @@
+name = "Lebron"
+age = 35
+salary = 37.44
+plays_basketball = True
+jersey_number = "23"
 
+print(name,age,salary,plays_basketball,jersey_number)
+type(name)
+type(age)
+type(plays_basketball)
